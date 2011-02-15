887d9507399c3f6e78b6dac4b611182db462b0fe
Adam Grant <adam.grant@sv.cmu.edu>
2011-02-14 21:18:48 -0800

    Initial commit following geocamDjangoAppSkeleton creation

---
+++ geocamCommon/tests.py
@@ -0,0 +1,15 @@
+# __BEGIN_LICENSE__
+# Copyright (C) 2008-2010 United States Government as represented by
+# the Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# __END_LICENSE__
+
+from django.test import TestCase
+
+
+class geocamCommonTest(TestCase):
+    """
+    Tests for geocamCommonWeb
+    """
+    def test_geocamCommon(self):
+        pass
